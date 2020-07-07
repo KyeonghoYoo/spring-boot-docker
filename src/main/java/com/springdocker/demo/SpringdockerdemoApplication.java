b9c8efa9b5fe020129f07134fb18aba49007f3c8
KyeonghoYoo <ykh6242@gmail.com>
2020-07-08 00:49:48 +0900

    Create SpringBootWeb project

---
+++ src/main/java/com/springdocker/demo/SpringdockerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springdocker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdockerdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdocker/demo/SpringdockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdocker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
